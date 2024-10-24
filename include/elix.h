889b179c913ad2c9cf3c069db189a869329a5706
robotsvision <robotsvision.vx@gmail.com>
2024-10-24 12:45:07 +0300

    [Elix]: As a submodule of Ellipse 2 framework.
    Minimum requrements were added.

--- include/elix.h
+++ include/elix.h
@@ -4,19 +4,29 @@
 /**************************************************************************//**
  * @file     elix.h
  * @brief    Elverse Corp. Elix graphical engine.
- * @version  V0.0.1
- * @date     09. October 2024
+ * @version  V0.0.2
+ * @date     24. October 2024
  * @author   Matvey Rybalkin
  ******************************************************************************/
 
-#ifdef __cplusplus
+#ifdef ELLIPSE_VERSION
+#if ELLIPSE_VERSION >= 2.0
+
+#ifdef ELLIPSE_LANGUAGE_CPP
 extern "C" {
 #endif 
 
+// Include every header file
 
-
-#ifdef __cplusplus
+#ifdef ELLIPSE_LANGUAGE_CPP
 }
 #endif 
+
+#else
+#   error "[ELIX] ERROR: The minimum required version of the Ellipse framework is 2.0. Download it from `https://github.com/ElverseCorp/llibs`"
+#endif
+#else
+#   error "[ELIX] ERROR: Must be used only with Ellipse framework. HELP: add #include <llibs.h> or #include <llibs.hpp>
+#endif
 
 #endif // ELIX_H_
